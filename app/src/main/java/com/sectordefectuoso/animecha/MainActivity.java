c170dbdf09b393899f56b878be5b03b79be2b1cf
Autopsia <autopsia9@gmail.com>
2018-06-01 18:06:45 -0500

    2

--- app/src/main/java/com/sectordefectuoso/animecha/MainActivity.java
+++ app/src/main/java/com/sectordefectuoso/animecha/MainActivity.java
@@ -2,12 +2,23 @@ package com.sectordefectuoso.animecha;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    TextView txtUser,txtPassword;
+    Button btnIngresar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void Ingresar(View view){
+        txtUser = findViewById(R.id.txtUser);
+        txtPassword = findViewById(R.id.txtPassword);
+        btnIngresar = findViewById(R.id.btnIngresar);
     }
 }
